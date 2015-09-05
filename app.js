bd5798dd352fb014eba634266a6d8762dfbe0de6
Julian Vargas <julianvargasalvarez@gmail.com>
2015-09-04 20:30:12 -0500

    adds basic express app

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var jsonWebToken = require('jsonwebtoken')
+var User = require('./models/user')
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var env = require('dotenv');
+var cors = require('cors');
+var jwt = require('express-jwt');
+var socket = require('./socket');
+
+env.load();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({secret: process.env.SECRET, resave: true, saveUninitialized: true}));
+app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res, next) {
+  return res.render('index.jade');
+});
+
+app.get('/sign-up', function(req, res, next) {
+  return res.render('sign-up.jade');
+});
+
+app.post('/login', function(req, res, next) {
+  res.redirect('/chat-room');
+});
+
+app.post('/sign-up', function(req, res, next) {
+  res.redirect('/chat-room');
+});
+
+app.get('/chat-room', function(req, res, next) {
+  return res.render('chat-room.jade');
+});
+
+http.listen(app.get('port'), function(){
+  socket(http);
+});
+
+module.exports = app;
